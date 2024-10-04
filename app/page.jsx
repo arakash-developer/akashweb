3f4a5a81d9a58c44de01dd5830b98d88b6c4a540
akash100a <arakash968786@gmail.com>
2024-10-04 16:22:40 +0600

    u

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+// import bg from "../public/bg.png"
+
+export default function Home() {
+  return (
+   <>
+    <div className="w-[100vw] h-[100vh] bg-[url('../public/bg.png')] bg-cover bg-center">
+      aa
+    </div>
+   </>
+  );
+}
